9f9b4cc400cde07b3ce42fc7d5157280d1234ea1
subhajit <subhajit.dinda@gmail.com>
2020-08-21 20:41:25 +0530

    code added for soap consumer

---
+++ src/test/java/com/learn/saopconsumer/SaopconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.saopconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaopconsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
